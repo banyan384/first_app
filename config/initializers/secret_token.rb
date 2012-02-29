f63eb1355bc5173bc9daf1eb87fdd39abb47951d
Banyan Tree <banyan384@gmail.com>
2012-02-28 19:59:24 -0600

    Intialize Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bb0fac61b6bc56b1e977fbcc07a3a25dc38a5c40f99613d9d915f693f3feeefbef15daa8d8451d603d3f75561899f3b28be56bee843a9da467ada6420f6338a1'
